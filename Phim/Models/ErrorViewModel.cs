749c534bc525749131b742944483804675633234
lhanh21102000 <anhb1805740@student.ctu.edu.vn>
2023-04-19 17:54:35 +0700

    Add project files.

---
+++ Phim/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Phim.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
